8b3761f8337e3519e75225eb9c9ce0ca3cd4a67e
Vanlimp <van.limap@gmail.com>
2024-09-07 10:42:59 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Minipizza de Calabresa",
+        descricao: "Deliciosa minipizza com sabor tradicional de calabresa, perfeita para um lanche rápido e saboroso.",
+        link: "https://guiadacozinha.com.br/receitas/minipizza-de-calabresa/",
+        tags: "salgada calabresa queijo"
+    },
+    {
+        titulo: "Minipizza de Frango",
+        descricao: "Minipizza com frango desfiado, catupiry e outros ingredientes que fazem dela uma opção irresistível.",
+        link: "https://guiadacozinha.com.br/receitas/minipizza-de-frango-com-catupiry/",
+        tags: "salgada frango catupiry"
+    },
+    {
+        titulo: "Minipizza de Banana com Canela",
+        descricao: "Minipizza doce com banana,  canela, queijo ideal para satisfazer o seu paladar uma delícia de sobremesa.",
+        link: "https://guiadacozinha.com.br/receitas/minipizza-de-banana-com-canela/",
+        tags: "doce banana com canela"
+    }
+];
